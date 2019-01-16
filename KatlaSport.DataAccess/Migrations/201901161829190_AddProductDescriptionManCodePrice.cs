95c6d540bc480ca239ff01d1b6c0120d41622cec
yuya-yuliya <yuya.yuliya@gmail.com>
2019-01-16 22:18:24 +0300

    Add Description, ManufacturerCode and Price property to CatalogueProduct and UpdateProductRequest model. Create a new migration AddProductDescriptionManCodePrice. Add new rules to UpdateProductRequestValidator.

---
+++ KatlaSport.DataAccess/Migrations/201901161829190_AddProductDescriptionManCodePrice.cs
@@ -0,0 +1,24 @@
+using System.Data.Entity.Migrations;
+
+namespace KatlaSport.DataAccess.Migrations
+{
+    /// <summary>
+    /// Add product description, manufacturer code and price migration.
+    /// </summary>
+    public partial class AddProductDescriptionManCodePrice : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.catalogue_products", "product_description", c => c.String(maxLength: 300));
+            AddColumn("dbo.catalogue_products", "product_manufacturer_code", c => c.String(nullable: false, maxLength: 10));
+            AddColumn("dbo.catalogue_products", "product_price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.catalogue_products", "product_price");
+            DropColumn("dbo.catalogue_products", "product_manufacturer_code");
+            DropColumn("dbo.catalogue_products", "product_description");
+        }
+    }
+}
