16e09b02a0d12636fd52d41eb55bdfcd807a1ab1
Unknown <yuya.yuliya@gmail.com>
2019-01-22 00:24:22 +0300

    Revert "Add fields Code and Name in HiveSection model."
    This reverts commit 697c060fbdbfd28b1e6176ca3caa623ef72eae17.

--- KatlaSport.Services.Models/HiveManagement/HiveSection.cs
+++ KatlaSport.Services.Models/HiveManagement/HiveSection.cs
@@ -8,16 +8,6 @@ namespace KatlaSport.Services.HiveManagement
     public class HiveSection : HiveSectionListItem
     {
         /// <summary>
-        /// Gets or sets a hive section name.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Gets or sets a hive section code.
-        /// </summary>
-        public string Code { get; set; }
-
-        /// <summary>
         /// Gets or sets a timestamp when the hive section was updated last time.
         /// </summary>
         public DateTime LastUpdated { get; set; }
