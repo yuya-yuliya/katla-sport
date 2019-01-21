26d30c78877c31ad9f3579bb1083bb8f2474103f
Unknown <yuya.yuliya@gmail.com>
2019-01-22 00:25:25 +0300

    Add UpdateHiveSectionRequest and UpdateHiveSectionRequestValidator.

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Attributes;
+
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a request for creating and updating a hive section.
+    /// </summary>
+    [Validator(typeof(UpdateHiveSectionRequestValidator))]
+    public class UpdateHiveSectionRequest
+    {
+        /// <summary>
+        /// Gets or sets a hive section name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive section code.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a validator for <see cref="UpdateHiveSectionRequestValidator"/>
+    /// </summary>
+    public class UpdateHiveSectionRequestValidator : AbstractValidator<UpdateHiveSectionRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateHiveSectionRequestValidator"/> class.
+        /// </summary>
+        public UpdateHiveSectionRequestValidator()
+        {
+            RuleFor(r => r.Name).Length(4, 60);
+            RuleFor(r => r.Code).Length(5);
+        }
+    }
+}
