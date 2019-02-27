73e8ee8c53ecaa69e0e80da426f5e8f3cb29c4cb
Unknown <yuya.yuliya@gmail.com>
2019-02-27 10:32:36 +0300

    Add commentaries.

--- KatlaSport.Services.Models/ProductManagement/IProductStoreService.cs
+++ KatlaSport.Services.Models/ProductManagement/IProductStoreService.cs
@@ -3,8 +3,16 @@ using System.Threading.Tasks;
 
 namespace KatlaSport.Services.ProductManagement
 {
+    /// <summary>
+    /// Represents a store product service.
+    /// </summary>
     public interface IProductStoreService
     {
+        /// <summary>
+        /// Gets a list of store products in hive section.
+        /// </summary>
+        /// <param name="hiveSectionId">An hive section id.</param>
+        /// <returns>A <see cref="Task{List{ProductCategoryListItem}}"/>.</returns>
         Task<List<ProductStoreItem>> GetProductStoreItemsAsync(int hiveSectionId);
     }
 }
--- KatlaSport.Services.Models/ProductManagement/ProductStoreItem.cs
+++ KatlaSport.Services.Models/ProductManagement/ProductStoreItem.cs
@@ -2,10 +2,19 @@
 {
     public class ProductStoreItem
     {
+        /// <summary>
+        /// Gets or sets a producr store item ID.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets a product.
+        /// </summary>
         public Product Product { get; set; }
 
+        /// <summary>
+        /// Gets or sets a quantity of items of certain product in the location.
+        /// </summary>
         public int Quantity { get; set; }
     }
 }
